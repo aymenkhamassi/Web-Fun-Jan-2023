978d4236581efed681b994bdaa0e4589f1132c16
aymenkhamassi <aymenkhammassi2@gmail.com>
2023-05-08 14:39:38 +0200

    update

--- Week2/Day 1/Practice/FizzBuzz/fizzbuzz.js
+++ Week2/Day 1/Practice/FizzBuzz/fizzbuzz.js
@@ -1,17 +1,22 @@
 for(var i=1;i<=100;i++){
-    if(i%3==0){
+    if((i%3==0)&&(i%5!=0)){
         console.log("fizz");
-        i++;
+        
     }
     else 
-    if(i%5==0){
+    if((i%5==0)&&(i%3!=0)){
         console.log("buzz");
-        i++;
+        
     }
-   
-    
-   
+    else
+    if((i%5 ==0)&&(i%3==0)){
+        console.log("fizzbuzz");
+        
+    }
+    else{
+        console.log(i);
 
-    console.log(i);
+    }
+    
 }
     
