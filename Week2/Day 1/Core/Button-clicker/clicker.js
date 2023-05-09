ce39295cd661015baa6b3c659915e9d40643f4ea
aymenkhamassi <aymenkhammassi2@gmail.com>
2023-05-09 10:16:37 +0200

    update

---
+++ Week2/Day 1/Core/Button-clicker/clicker.js
@@ -0,0 +1,12 @@
+function clicked(element){
+    console.log("i ve been clicked");
+    element.innerText="Logout";
+}
+
+function removed(element){
+    element.remove();
+}
+
+function alert(element){
+    
+}
