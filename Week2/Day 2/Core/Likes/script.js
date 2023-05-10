2342a79c94f8e8eb09dd76637851c68dea539ef0
aymenkhamassi <aymenkhammassi2@gmail.com>
2023-05-10 16:00:40 +0200

    update

---
+++ Week2/Day 2/Core/Likes/script.js
@@ -0,0 +1,28 @@
+var count = 9;
+var countElement=document.querySelector('#count');
+console.log(countElement);
+
+
+function add1(){
+    count++;
+    countElement.innerText=count+ " Like(s)";
+}
+
+var count1 = 12;
+var count1Element=document.querySelector('#count1');
+console.log(count1Element);
+
+function add12(){
+    count1++;
+    count1Element.innerText=count1+ " Like(s)";
+}
+
+
+var count2 = 9;
+var count2Element=document.querySelector('#count2');
+console.log(count2Element);
+
+function add13(){
+    count2++;
+    count2Element.innerText=count2+ " Like(s)";
+}
