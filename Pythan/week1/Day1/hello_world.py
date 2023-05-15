ff9af1c33ab34329754a66172462941051093720
aymenkhamassi <aymenkhammassi2@gmail.com>
2023-05-15 15:50:31 +0200

    pushed

---
+++ Pythan/week1/Day1/hello_world.py
@@ -0,0 +1,7 @@
+print("hello world")
+name = "aymen"
+print("hello " + name)
+print("hello aymen")
+number = 70
+print("hello " + str(number))
+print(f"hello {name} and my favorit number is {number}")
